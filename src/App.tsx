33c9f2af5787d42d864f930450a7b89b7690c6cf
kaczormp4 <kaczkaoryginal@gmail.com>
2025-02-01 18:31:22 +0100

    removed odl files

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import PageScroller from "react-page-scroller";
-
 import "./App.css";
-import Scene from "./components/game/Scene/Scene";
 
 function App() {
-  return <Scene />;
-  return (
-    <PageScroller animationTimer={500}>
-      <div style={{ height: "100vh", backgroundColor: "lightblue" }}>xd1</div>
-      <div style={{ height: "100vh" }}>
-        <Scene />
-      </div>
-    </PageScroller>
-  );
+  return <div>xd2</div>;
 }
 
 export default App;
